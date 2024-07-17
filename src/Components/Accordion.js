612bb217a2a023dd179ed07600afffcaee191399
pouya-kh <pouyawebdeveloper@gmail.com>
2024-07-17 22:29:00 +0330

    Final-version-Accordion

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -9,14 +9,16 @@ function Accordion({ question, num }) {
       <div
         id="question"
         onClick={() => setIsOpen(!isOpen)}
-        className={`flex items-center gap-5 text-2xl text-center font-sans font-medium tracking-tight px-6 py-8 bg-gray-50 rounded-md shadow-lg shadow-gray-600/30 cursor-pointer text-zinc-900 capitalize mb-2 border-t-4 border-gray-50 transition-all duration-300 ${
-          isOpen && "text-green-600 border-t-4 border-green-600"
+        className={`flex items-center gap-5 text-2xl font-sans font-medium tracking-tight px-6 py-8 bg-gray-50 rounded-md shadow-lg shadow-gray-600/30 cursor-pointer text-zinc-900 capitalize mb-2 border-t-4 border-gray-50 transition-all duration-300 ${
+          isOpen && "!text-green-600 border-t-4 !border-green-600"
         }`}
       >
-        <span className={`text-zinc-400 ${isOpen && "text-green-600"}`}>
+        <span className={`text-zinc-400 ${isOpen && "!text-green-600"}`}>
           {num + 1 < 10 ? `0${num + 1}` : num + 1}
         </span>
-        <p className="mr-auto">{question.question}</p>
+        <p className="mr-auto w-full hyphens-auto break-all">
+          {question.question}
+        </p>
         <Svg state={isOpen}></Svg>
       </div>
       <div
